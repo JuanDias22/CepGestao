efc0f7c5b3afeecb5d10cf099cfa0974248e3b81
Juan Dias <fdias.juan@gmail.com>
2026-05-10 20:32:48 -0300

    fix: ajuste da coluna complemento para permitir valores nulos

---
+++ CepGestao/Migrations/20260510232748_ComplementoOpcional.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CepGestao.Migrations
+{
+    /// <inheritdoc />
+    public partial class ComplementoOpcional : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Complemento",
+                table: "Enderecos",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Complemento",
+                table: "Enderecos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
