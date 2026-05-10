6ae4d0b7bf369e5b2f3702d3ba189b5705101084
Juan Dias <fdias.juan@gmail.com>
2026-05-10 18:48:09 -0300

    feat: criação das entidades Usuario e Endereco

--- CepGestao/Models/Endereco.cs
+++ CepGestao/Models/Endereco.cs
@@ -2,6 +2,24 @@
 {
     public class Endereco
     {
-       
+        public int Id { get; set; }
+
+        public string Cep { get; set; }
+
+        public string Logradouro { get; set; }
+
+        public string Complemento { get; set; }
+
+        public string Bairro { get; set; }
+
+        public string Cidade { get; set; }
+
+        public string Uf { get; set; }
+
+        public string Numero { get; set; }
+
+        public int UsuarioId { get; set; }
+
+        public Usuario Usuario { get; set; }
     }
 }
--- CepGestao/Models/Usuario.cs
+++ CepGestao/Models/Usuario.cs
@@ -2,6 +2,14 @@
 {
     public class Usuario
     {
-       
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string User { get; set; }
+
+        public string Senha { get; set; }
+
+        public List<Endereco> Enderecos { get; set; }
     }
 }
