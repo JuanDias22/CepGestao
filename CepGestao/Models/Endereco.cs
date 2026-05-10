050f064ed54b71f3b9841682492377bf1122f9df
Juan Dias <fdias.juan@gmail.com>
2026-05-10 20:31:56 -0300

    fix: correção para complemento receber nulo

--- CepGestao/Models/Endereco.cs
+++ CepGestao/Models/Endereco.cs
@@ -8,7 +8,7 @@
 
         public string Logradouro { get; set; }
 
-        public string Complemento { get; set; }
+        public string? Complemento { get; set; }
 
         public string Bairro { get; set; }
 
