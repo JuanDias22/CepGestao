52725513156e0fc5546352ea2baba4a317b300bb
Juan Dias <fdias.juan@gmail.com>
2026-05-10 18:58:01 -0300

    feat: implementação da autenticação de usuários

--- CepGestao/Controllers/AuthController.cs
+++ CepGestao/Controllers/AuthController.cs
@@ -19,6 +19,25 @@ namespace CepGestao.Controllers
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Login(string username, string senha)
+        {
+            var usuario = _context.Usuarios
+                .FirstOrDefault(x =>
+                    x.User == username &&
+                    x.Senha == senha);
     
+            if (usuario == null)
+            {
+                ViewBag.Erro = "Usuário ou senha inválidos";
+                return View();
+            }
+
+            HttpContext.Session.SetInt32("UsuarioId", usuario.Id);
+
+            var usuarioId = HttpContext.Session.GetInt32("UsuarioId");
+
+            return Content($"Usuário logado: {usuarioId}");
+        }
     }
 }
